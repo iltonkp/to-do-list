623493be716539ec64b482b3380c0f110ee6f224
iltonkp <iltonk.si@gmail.com>
2023-08-16 23:08:04 +0100

    feat: Change task domain model id to nullable

--- src/main/kotlin/com/todolist/domain/models/Task.kt
+++ src/main/kotlin/com/todolist/domain/models/Task.kt
@@ -4,7 +4,7 @@ package com.todolist.domain.models
 import java.time.LocalDate
 
 data class Task(
-    val id: String,
+    val id: String? = null ,
     val title: String? = null,
     val description: String,
     val isActive: Boolean = true,
