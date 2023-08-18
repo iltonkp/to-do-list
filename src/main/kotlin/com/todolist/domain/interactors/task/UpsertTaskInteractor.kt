79f2fa00501670cefc9ae7a4af801d88005e1d1e
iltonkp <iltonk.si@gmail.com>
2023-08-18 17:30:53 +0100

    feat: Refactoring task interactor class name

--- src/main/kotlin/com/todolist/domain/interactors/task/UpsertTaskInteractor.kt
+++ src/main/kotlin/com/todolist/domain/interactors/task/UpsertTaskInteractor.kt
@@ -1,15 +1,21 @@
 package com.todolist.domain.interactors.task
 
+import com.todolist.domain.exceptions.InvalidDateException
 import com.todolist.domain.interactors.Interactor
 import com.todolist.domain.models.Task
 import com.todolist.domain.ports.TaskRepositoryPort
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
-class CreateTaskInteractor(
+class UpsertTaskInteractor(
     private val taskRepositoryPort: TaskRepositoryPort
 ) : Interactor<Task, Unit> {
     override fun execute(request: Task) {
+
+        if (request.date.isBefore(LocalDate.now()))
+            throw InvalidDateException("The task date can not be before today.")
+
         taskRepositoryPort.save(request)
     }
 
