7d4cbe91f7e5302c19a5bb1b0f79b4c7bf830ed5
iltonkp <iltonk.si@gmail.com>
2023-08-26 16:21:06 +0100

    refactor: refactoring TaskDto to remove date format and include jsonInclude annotation

--- src/main/kotlin/com/todolist/adapters/rest/dto/TaskDto.kt
+++ src/main/kotlin/com/todolist/adapters/rest/dto/TaskDto.kt
@@ -1,7 +1,7 @@
 package com.todolist.adapters.rest.dto
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import com.todolist.domain.models.Task
@@ -9,12 +9,12 @@ import java.time.LocalDate
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class TaskDto(
     val id: String? = null,
     val title: String? = null,
     val description: String,
     val isActive: Boolean = true,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     val date: LocalDate
 )
 
